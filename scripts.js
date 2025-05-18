71138450484be34fa2b0610477e27aac6d6f8068
pigstain <edelwiss2@gmail.com>
2025-05-18 14:00:24 +0300

    project finished

---
+++ scripts.js
@@ -0,0 +1,119 @@
+let previusInput='';
+let currentInput="";
+let operator = "";
+const screen = document.querySelector(".screen");
+let numbers = document.querySelectorAll(".number");
+
+numbers.forEach(number=>{
+    number.addEventListener("click",()=>{
+        if(previusInput===""){
+        screen.textContent=screen.textContent+number.textContent;
+        }
+        else{
+            screen.textContent=screen.textContent+number.textContent;
+            currentInput=currentInput+number.textContent;
+            
+        }
+    })
+})
+let operators = document.querySelectorAll(".operator");
+
+operators.forEach(oper=>{
+    oper.addEventListener("click",()=>{
+        if(previusInput===""){
+        previusInput=screen.textContent;
+        operator=oper.textContent;
+        screen.textContent=screen.textContent+oper.textContent;
+        }
+        else if(previusInput!==""){
+            if(operator==="+"){
+                previusInput=Number(previusInput)+Number(currentInput);
+                
+                operator=oper.textContent;
+                screen.textContent=previusInput+operator;
+                currentInput="";
+            }
+            else if(operator==="-"){
+                previusInput=Number(previusInput)-Number(currentInput);
+                
+                operator=oper.textContent;
+                screen.textContent=previusInput+operator;
+                currentInput="";
+            }
+            else if(operator==="*"){
+                previusInput=Number(previusInput)*Number(currentInput);
+                
+                operator=oper.textContent;
+                screen.textContent=previusInput+operator;
+                currentInput="";
+            }
+            else if(operator==="/"){
+                previusInput=Number(previusInput)/Number(currentInput);
+                
+                operator=oper.textContent;
+                screen.textContent=previusInput+operator;
+                currentInput="";
+            }
+            else{
+                screen.textContent=screen.textContent+oper.textContent;
+                operator=oper.textContent;
+            }
+        }
+    })
+})
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click",()=>{
+    screen.textContent="";
+    previusInput="";
+    currentInput="";
+    operator="";
+})
+
+const operate  = document.querySelector(".operate");
+operate.addEventListener("click",()=>{
+    if(previusInput===""){
+        alert("Error");
+    }
+else if(previusInput!==""){
+    if(operator==="+"){
+                previusInput=Number(previusInput)+Number(currentInput);
+                
+                
+                operator="";
+                screen.textContent=previusInput;
+                currentInput="";
+                
+                
+            }
+            else if(operator==="-"){
+                previusInput=Number(previusInput)-Number(currentInput);
+                
+                
+                operator="";
+                screen.textContent=previusInput;
+                currentInput="";
+                
+            }
+            else if(operator==="*"){
+                previusInput=Number(previusInput)*Number(currentInput);
+                
+               
+                operator="";
+                screen.textContent=previusInput;
+                currentInput="";
+                
+                
+            }
+            else if(operator==="/"){
+                previusInput=Number(previusInput)/Number(currentInput);
+                
+                
+                operator="";
+                screen.textContent=previusInput;
+                currentInput="";
+                
+                
+            }
+}
+})
